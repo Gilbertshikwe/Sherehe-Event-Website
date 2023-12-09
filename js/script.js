865e19397efe777713d71279bd91ba19cea68732
Gilbert <shikweg@gmail.com>
2023-12-09 09:46:01 +0300

    added a javascript function to the button

---
+++ js/script.js
@@ -0,0 +1,4 @@
+function changetext() {
+    document.getElementById('btn1').innerHTML = 
+   "Date: Sat 9 Dec 2023" + "<br>" + "Time: 2.00pm to 12.00am" + "<br>" + "Location: Diani Reef Beach Hotel" 
+}
