8f97276590315de3df51bcba15595fb8c54237b8
Gilbert <shikweg@gmail.com>
2023-12-09 10:55:53 +0300

    added javascript functions to all the buttons events

--- js/script.js
+++ js/script.js
@@ -1,4 +1,60 @@
 function changetext() {
     document.getElementById('btn1').innerHTML = 
-   "Date: Sat 9 Dec 2023" + "<br>" + "Time: 2.00pm to 12.00am" + "<br>" + "Location: Diani Reef Beach Hotel" 
+   "Date: Sat 9 Dec 2023" + "<br>" + "Time: 2:00PM - 12:00AM" + "<br>" + "Location: Diani Reef Beach Hotel" + "<br>" + "Host : Capital Group Limited"
+}
+function changetext1() {
+    document.getElementById('btn2').innerHTML =
+    "Date: Sat 9 Dec 2023" + "<br>" + "Time: 06:30PM - 11:00 PM" + "<br>" + "Location: Braeburn Theatre" + "<br>" + "Host : ENGAGE KENYA LIMITED"
+}
+function changetext2() {
+    document.getElementById('btn3').innerHTML = 
+    "Date: Sat 16 Dec 2023" + "<br>" + "Time: 08:00 AM - 06:00 PM" +"<br>"+ "Location: Rift Valley Sports Club"+ "<br>" + "Host : Wess entertainment and events LTD"
+}
+function changetext3() {
+    document.getElementById('btn4').innerHTML =
+    "Date: Tue 12 Dec 2023" + "<br>" + "Time: 12:00 PM - 06:00 PM"  + "Location: Waterfront Karen"+ "<br>" + "Host : ADVANTAGE AFRICA INVESTMENT LIMITED COMPANY"
+}
+function changetext4() {
+    document.getElementById('btn5').innerHTML = 
+    "Date: Sat 9 Dec 2023" + "<br>"+"Time: 07:00 AM - 01:00 PM"+"<br>"+"Location: Vipingo Ridge" +"<br>"+ "Host : Vipingo Ridge Limited"
+}
+function changetext5() {
+    document.getElementById('btn6').innerHTML = 
+    "Date: Sat 9 Dec 2023" + "<br>" + " Time: 06:00 PM - 11:59 PM" + "<br>"+ "Location: The Junction Mall" + "<br>" + "Host : Atemi Oyungu"
+}
+function changetext6() {  
+    document.getElementById('btn7').innerHTML = 
+    "Date: Fri 15 Dec 2023" + "<br>"+" Time: 06:00 PM - 06:00 AM" + "<br>"+
+    "Location: Carnivore Simba Saloon, Nairobi"+"<br>"+
+    "Host : DJ ADRIAN"
+}
+function changetext7() {
+    document.getElementById('btn8').innerHTML = 
+    "Date: Sat 16 Dec 2023" + "<br>" + "Time: 07:00 PM - 11:59 PM"+"<br>"+
+    "Location: Braebun Theatre, Gitanga Road"+"<br>"+
+    "Host : Oh Kay Honey"
+}
+function changetext8() {
+    document.getElementById('btn9').innerHTML =
+    "Date: Sun 17 Dec 2023" + "<br"+ "Time: 12:00 PM - 06:00 PM"+"<br>"+
+    "Location: The Nairobi Arboretum"+"<br>"+
+    "Host : Key Marketing"
+}
+function changetext9() {
+    document.getElementById('btn10').innerHTML = 
+    "Date: Fri 29 Dec 2023" + "<br>" + "Time: 12:00 PM - 11:59 PM"+"<br>"+
+    "Location: Woza Woza Camping and Funzone,Diani"+"<br>"+
+    "Host : Gig Factory Agency"
+}
+function changetext10() {
+    document.getElementById('btn11').innerHTML = 
+    "Date: Tue 26 Dec 2024" + "<br>" + "Time: 08:00 AM - 05:00 PM" + "<br>"+
+    "Location: Loita Hall, Sarit Expo Centre, Westlands"+"<br>"+
+    "Host : Drone Space Ltd"
+}
+function changetext11() {
+    document.getElementById('btn12').innerHTML =
+    "Date: Fri 15 Dec 2024" + "<br>"+" Time: 12:00 PM - 11:59 PM"+"<br>"+
+    "Location: Freedom City, Entebbe Road"+"<br>"+
+    "Host : INTERNATIONAL CITY DESIGNERS LIMITED"
 }
